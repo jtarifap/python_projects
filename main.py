29af2c39f30c6ca2176f8357c8096d0b934df504
jtarifap <jtarifa@gmail.com>
2022-05-02 16:36:39 +0200

    SubidaFiles1

---
+++ main.py
@@ -0,0 +1,44 @@
+#prueba de acceso SQL Server
+import pyodbc
+""" from multiprocessing import Manager
+from multiprocessing.spawn import import_main_path """
+from tkinter import *
+
+from genericClass import generic
+
+"""
+server = 'SERVINTRA01\SQLINTRA'
+database = 'VVTecnics_TEST'
+username = 'sa'
+password = '*****'
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server +
+                      ';DATABASE='+database+';UID='+username+';PWD=' + password)
+"""""
+server = 'SERVINTRA01\SQLINTRA'
+database = 'VVTecnics_TEST'
+username = 'sa'
+password = '***'
+
+gen = generic.genericManage
+
+def testSelect():
+    cnxn = gen.conSQL(server, database, username, password)
+
+
+   # cnxn = ConSQL(server, database, username, password)
+    cursor = cnxn.cursor()
+    fecha = ""
+    # Sample select query
+    cursor.execute("SELECT * from facturas")
+    row = cursor.fetchone()
+    while row:
+        va1 = row[0]
+        va2 = row[1]
+        va3 = row[2]
+        va4 = row[3]
+        #print(str(va1))
+        print(str(va1) + ' --- ' + str(va2) +
+              ' --- ' + str(va3) + ' --- ' + str(va4))
+        row = cursor.fetchone()
+
+testSelect()
