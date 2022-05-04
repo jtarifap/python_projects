5bdaa6b7292ea199b5f0fb6223e14bdd57d9a35f
jtarifap <jtarifa@gmail.com>
2022-05-04 12:55:32 +0200

    actualizacion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #prueba de acceso SQL Server
+from unittest.util import sorted_list_difference
 import pyodbc
 """ from multiprocessing import Manager
 from multiprocessing.spawn import import_main_path """
@@ -7,26 +8,17 @@ from tkinter import *
 from genericClass import generic
 from dbaccess import db
 
-"""
-server = 'SERVINTRA01\SQLINTRA'
-database = 'VVTecnics_TEST'
-username = 'sa'
-password = '*****'
-cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server +
-                      ';DATABASE='+database+';UID='+username+';PWD=' + password)
-"""""
-#username = 'sa'
-#password = '***'
-server = db.server
-database = db.dbname
-username = db.us
-password = db.passw
-
+bd = db()
+server = bd.server
+database = bd.dbname
+username = bd.us
+password = bd.passw
+print(bd)
 gen = generic.genericManage
 
+ 
 def testSelect():
     cnxn = gen.conSQL(server, database, username, password)
-
 
    # cnxn = ConSQL(server, database, username, password)
     cursor = cnxn.cursor()
@@ -40,8 +32,9 @@ def testSelect():
         va3 = row[2]
         va4 = row[3]
         #print(str(va1))
+
         print(str(va1) + ' --- ' + str(va2) +
               ' --- ' + str(va3) + ' --- ' + str(va4))
         row = cursor.fetchone()
 
-testSelect()
+print(testSelect())
